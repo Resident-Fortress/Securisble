c7d21c87d27930f42d669643ad9a179601dc01ee
Matthew D <42253757+Resident-Fortress@users.noreply.github.com>
2024-03-12 15:50:02 -0500

    Fixed powercfg

--- securshell.ps1
+++ securshell.ps1
@@ -3,7 +3,7 @@
 # Updated infrequently. Commands may damage your system I am not liable for any damages done to machines. 
 
 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
-
+powercfg /s SCHEME_MIN
 
 
 winget install -e --id Google.Chrome
