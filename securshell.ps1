e85ec49676dfdd1aa50d818346c4cd962f7aea38
Matthew D <42253757+Resident-Fortress@users.noreply.github.com>
2024-06-29 08:50:15 -0500

    Added more comments into the file

--- securshell.ps1
+++ securshell.ps1
@@ -33,52 +33,141 @@ Start-Service -Name "wuauserv" # Windows Update Service start
 #Set-Service -Name "ftpsvc" -StartupType "Disabled" // Not needed in standard Windows that doesn't run IIS
 
 # Task 6 Registry edits
+
+# Disables the Windows Optional Feature SMBv1 protocol
 powershell.exe Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol -norestart 
+# Disable SMBv1 protocol
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\mrxsmb10" /v Start /t REG_DWORD /d 4 /f
+
+# Disable SMBv1 on LanmanServer
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v SMB1 /t REG_DWORD /d 0 /f
+
+# Restrict null session access on LanmanServer
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters" /v RestrictNullSessAccess /t REG_DWORD /d 1 /f
+
+# Restrict anonymous SAM access
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RestrictAnonymousSAM /t REG_DWORD /d 1 /f
+
+# Restrict anonymous access
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RestrictAnonymous /t REG_DWORD /d 1 /f
+
+# Disable EveryoneIncludesAnonymous
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v EveryoneIncludesAnonymous /t REG_DWORD /d 0 /f
+
+# Restrict remote SAM access
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RestrictRemoteSAM /t REG_SZ /d "O:BAG:BAD:(A;;RC;;;BA)" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v UseMachireneId /t REG_DWORD /d 1 /f
+
+# Enable machine SID
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v UseMachineId /t REG_DWORD /d 1 /f
+
+# Limit blank password use
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+
+# Disable null session fallback
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\LSA\MSV1_0" /v allownullsessionfallback /t REG_DWORD /d 0 /f
+
+# Enable security signature on LanmanWorkstation
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" /v EnableSecuritySignature /t REG_DWORD /d 1 /f
+
+# Enable RPC traffic encryption
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v fEncryptRPCTraffic /t REG_DWORD /d 1 /f
+
+# Disable command prompt in Terminal Services
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v fDisableCdm /t REG_DWORD /d 1 /f
+
+# Disable autoplay for non-volume devices
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoAutoplayfornonVolume /t REG_DWORD /d 1 /f
+
+# Disable drive type autorun
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer" /v NoDriveTypeAutoRun /t REG_DWORD /d 255 /f
+
+# Disable autorun
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoAutorun /t REG_DWORD /d 1 /f
+
+# Enable SmartScreen
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 1 /f
+
+# Set SmartScreen level to Block
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v ShellSmartScreenLevel /t REG_SZ /d Block /f
+
+# Enable secure channel sealing
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" /v SealSecureChannel /t REG_DWORD /d 1 /f
+
+# Enable secure channel signing
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" /v SignSecureChannel /t REG_DWORD /d 1 /f
+
+# Enable advanced protection in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "AdvancedProtectionAllowed" /t REG_DWORD /d 1 /f
+
+# Enable DNS over HTTPS in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "DnsOverHttpsMode" /t REG_SZ /d on /f
+
+# Set minimum SSL version to TLS 1.0 in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "SSLVersionMin" /t REG_SZ /d tls1 /f
+
+# Disable screen capture in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "ScreenCaptureAllowed" /t REG_DWORD /d 0 /f
+
+# Enable site per process in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "SitePerProcess" /t REG_DWORD /d 1 /f
+
+# Enable TLS 1.3 hardening for local anchors in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "TLS13HardeningForLocalAnchorsEnabled" /t REG_DWORD /d 1 /f
+
+# Disable video capture in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "VideoCaptureAllowed" /t REG_DWORD /d 0 /f
+
+# Disable ambient authentication in private modes in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "AmbientAuthenticationInPrivateModesEnabled" /t REG_DWORD /d 0 /f
+
+# Disable audio capture in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "AudioCaptureAllowed" /t REG_DWORD /d 0 /f
+
+# Enable audio sandboxing in Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "AudioSandboxEnabled" /t REG_DWORD /d 1 /f
 
 # Task 7 Auditiing of system events that are critical
+
+# Enable auditing for Security Group Management with both success and failure events
 Auditpol /set /subcategory:"Security Group Management" /success:enable /failure:enable
+
+# Enable auditing for Process Creation with both success and failure events
 Auditpol /set /subcategory:"Process Creation" /success:enable /failure:enable
+
+# Enable auditing for Logoff with success events only and disable failure events
 Auditpol /set /subcategory:"Logoff" /success:enable /failure:disable
-Auditpol /set /subcategory:"Logon" /success:enable /failure:enable 
+
+# Enable auditing for Logon with both success and failure events
+Auditpol /set /subcategory:"Logon" /success:enable /failure:enable
+
+# Disable auditing for Filtering Platform Connection both success and failure events
 Auditpol /set /subcategory:"Filtering Platform Connection" /success:enable /failure:disable
+
+# Enable auditing for Removable Storage with both success and failure events
 Auditpol /set /subcategory:"Removable Storage" /success:enable /failure:enable
+
+# Disable auditing for SAM both success and failure events
 Auditpol /set /subcategory:"SAM" /success:disable /failure:disable
+
+# Disable auditing for Filtering Platform Policy Change both success and failure events
 Auditpol /set /subcategory:"Filtering Platform Policy Change" /success:disable /failure:disable
+
+# Enable auditing for IPsec Driver with both success and failure events
 Auditpol /set /subcategory:"IPsec Driver" /success:enable /failure:enable
+
+# Enable auditing for Security State Change with both success and failure events
 Auditpol /set /subcategory:"Security State Change" /success:enable /failure:enable
+
+# Enable auditing for Security System Extension with both success and failure events
 Auditpol /set /subcategory:"Security System Extension" /success:enable /failure:enable
+
+# Enable auditing for System Integrity with both success and failure events
 Auditpol /set /subcategory:"System Integrity" /success:enable /failure:enable
-auditpol /set /category:"System","Account Management","Account Logon","Logon/Logoff","Policy Change" /failure:enable /success:enable  
+
+# Enable auditing for System, Account Management, Account Logon, Logon/Logoff, and Policy Change categories with both success and failure events
+auditpol /set /category:"System","Account Management","Account Logon","Logon/Logoff","Policy Change" /failure:enable /success:enable
+
+# Enable auditing for DS Access and Object Access categories with failure events only
 auditpol /set /category:"DS Access","Object Access" /failure:enable
 
 # Task 8 Set firewall profiles to true
@@ -149,13 +238,13 @@ netsh advfirewall firewall add rule name="Block wmic.exe netconns" program="%sys
 netsh advfirewall firewall add rule name="Block wmic.exe netconns" program="%systemroot%\SysWOW64\wbem\wmic.exe" protocol=tcp dir=out enable=yes action=block profile=any
 netsh advfirewall firewall add rule name="Block wscript.exe netconns" program="%systemroot%\system32\wscript.exe" protocol=tcp dir=out enable=yes action=block profile=any
 netsh advfirewall firewall add rule name="Block wscript.exe netconns" program="%systemroot%\SysWOW64\wscript.exe" protocol=tcp dir=out enable=yes action=block profile=any
-netsh Advfirewall set allprofiles state on
 
 # Task 12 Install the MS Powershell updater for easier and quicker method of updateing windows
+# Task 12 Install the MS Powershell updater for easier and quicker method of updating windows
 Install-Module PSWindowsUpdate
 Add-WUServiceManager -MicrosoftUpdate
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\($env.computername-Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force
 
-# Task 13 The machine creates a restore point after the machine is ready and lock powershell execution
+# Task 13 The machine creates a restore point after the machine is ready and locks PowerShell execution
 Checkpoint-Computer -Description "Post Script"
 Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine
